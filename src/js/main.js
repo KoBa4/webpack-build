358532c61aca590c7ff31c8848b969400c04118f
KoBa4 <cova44ever@gmail.com>
2020-12-30 08:09:57 +0600

    fix main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import '../../node_modules/focus-visible/dist/focus-visible'
 import '../scss/main.scss'
-// import '../index.html'
 
 if (module.hot) {
   module.hot.accept() // already had this init code
@@ -9,13 +8,3 @@ if (module.hot) {
     if (status === 'prepare') console.clear()
   })
 }
-
-class Game {
-  constructor() {
-    // eslint-disable-next-line no-unused-vars
-    let name = 'Violin Charades'
-  }
-}
-// eslint-disable-next-line no-unused-vars
-const myGame = new Game()
-console.log('test')
